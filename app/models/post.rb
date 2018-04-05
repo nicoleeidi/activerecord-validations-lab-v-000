0a3fdaa576e549754fad746b2803745b7496d052
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-05 20:55:20 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum:250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)
 end
+
 class MyValidator < ActiveModel::Validator
   def validate(post)
     validates :title, inclusion:  { in: %w( "Won't Believe", "Secret", "Top [0-9]", or "Guess")
