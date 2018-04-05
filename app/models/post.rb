891b24e82b76e1206d8fe313e0f337726e6c2de8
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-05 20:57:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,20 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates_with MyValidator
+
   validates :title, presence: true
   validates :content, length: {minimum:250}
   validates :summary, length: {maximum:250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)
 
     validate :is_clickbait?
-    
+
       CLICKBAIT_PATTERNS = [
         /Won't Believe/i,
         /Secret/i,
         /Top [0-9]*/i,
         /Guess/i
       ]
-    
+
       def is_clickbait?
         if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
           errors.add(:title, "must be clickbait")
