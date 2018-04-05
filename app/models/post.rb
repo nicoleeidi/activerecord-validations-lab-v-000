4d627292add50ce67c15b0b08eb763115628ca56
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-05 20:41:03 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true 
+  validates :title, presence: true
   validates :content, length: {minimum:250}
   validates :summary, length: {maximum:250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)
 end
-class MyValidator < ActiveRecord::Validator 
-end 
+class MyValidator < ActiveRecord::Validator
+  def validate(post)
+  end
+end
